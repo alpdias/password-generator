cebfa46e774c31072284ae51aa7ba9d38553bad6
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-11-26 12:50:02 -0300

    Update pwd.py

--- src/pwd.py
+++ src/pwd.py
@@ -16,18 +16,18 @@ def generator(size):
     \n:return: Password
     """
     
-    numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    letrasMin = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z']
-    letrasMax = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'W', 'Y', 'Z']
-    simbolos = ['!', '@', '#', '$', '%', '&', '*']
+    numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] # list of numbers
+    letrasMin = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z'] # lowercase list
+    letrasMax = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'W', 'Y', 'Z'] # list of capital letters
+    simbolos = ['!', '@', '#', '$', '%', '&', '*'] # symbol list
 
     lista = []
 
-    lista.append(numeros + letrasMin + letrasMax + simbolos)
+    lista.append(numeros + letrasMin + letrasMax + simbolos) # unique list for all characters
 
     senha = ''
 
     for i in range(size):
-        senha += random.choice(lista[0])
+        senha += random.choice(lista[0]) # choice of characters
 
     return senha
