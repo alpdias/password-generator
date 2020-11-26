3515455dcda4f268440c9b73d3e8bfe53d158565
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-11-26 12:43:28 -0300

    Update pwd.py

--- src/pwd.py
+++ src/pwd.py
@@ -1,17 +1,33 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created in 11/2020
+@Author: Paulo https://github.com/alpdias
+'''
+
+# imported libraries
 import random
 
-numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-letrasMin = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z']
-letrasMax = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'W', 'Y', 'Z']
-simbolos = ['!', '@', '#', '$', '%', '&', '*']
+def generator(size):
+    
+    """
+    -> Simple password generator\
+    \n:param size: Password length
+    \n:return: Password
+    """
+    
+    numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    letrasMin = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z']
+    letrasMax = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'W', 'Y', 'Z']
+    simbolos = ['!', '@', '#', '$', '%', '&', '*']
 
-lista = []
+    lista = []
 
-lista.append(numeros + letrasMin + letrasMax + simbolos)
+    lista.append(numeros + letrasMin + letrasMax + simbolos)
 
-senha = ''
+    senha = ''
 
-for i in range(8):
-    senha += random.choice(lista[0])
+    for i in range(size):
+        senha += random.choice(lista[0])
 
-print(senha)
+    return senha
